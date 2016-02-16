75dfc9017278c1be746079b369331d6d0f9340d2
lucastherrien <lucastherrien@172.17.4.146>
2016-02-16 17:24:15 -0500

    untested: added auto drive to defense

--- Robot2016/src/org/usfirst/frc/team4561/robot/commands/AutoDTD.java
+++ Robot2016/src/org/usfirst/frc/team4561/robot/commands/AutoDTD.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4561.robot.commands;
 
+import org.usfirst.frc.team4561.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -18,6 +20,7 @@ public class AutoDTD extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.driveTrain.driveArcade(1, 0); //TODO: verify
     }
 
     // Make this return true when this Command no longer needs to run execute()
