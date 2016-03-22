30479e86875eb619ab64478c828a6417899d7c5f
lucastherrien <lucastherrien@172.17.3.46>
2016-03-22 18:23:24 -0400

    Untested: sorry, my last push had an empty command group

--- Robot2016/src/org/usfirst/frc/team4561/robot/automodes/AutoLowbarShootHigh.java
+++ Robot2016/src/org/usfirst/frc/team4561/robot/automodes/AutoLowbarShootHigh.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc.team4561.robot.automodes;
 
+import org.usfirst.frc.team4561.robot.commands.DriveArcadeTimed;
+import org.usfirst.frc.team4561.robot.commands.Fire;
+import org.usfirst.frc.team4561.robot.commands.PIDGoalAlign;
+import org.usfirst.frc.team4561.robot.commands.ShooterAlign;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -8,21 +13,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoLowbarShootHigh extends CommandGroup {
     
     public  AutoLowbarShootHigh() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	// Align to goal prefer right
+    	addSequential(new DriveArcadeTimed(0, 1, 1)); //TODO: must verify values
+    	// Spin up shooter
+    	addSequential(new ShooterAlign());
+    	// Fire
+    	addSequential(new Fire());
+    	// Realign to drive backwards through low bar
+    	addSequential(new DriveArcadeTimed(0, -1, 1)); //TODO: Let us test FIRST!!
+    	// Drives backwards through low bar
+    	addSequential(new DriveArcadeTimed(-1, 0, 3));
     }
 }
