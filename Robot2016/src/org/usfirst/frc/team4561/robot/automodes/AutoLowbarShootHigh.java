029f0d89fd81102ab1026186d1239a12ed0761c4
lucastherrien <lucastherrien@172.17.3.46>
2016-03-24 16:53:21 -0400

    Untested: just polishing the previously added automode

--- Robot2016/src/org/usfirst/frc/team4561/robot/automodes/AutoLowbarShootHigh.java
+++ Robot2016/src/org/usfirst/frc/team4561/robot/automodes/AutoLowbarShootHigh.java
@@ -1,8 +1,11 @@
 package org.usfirst.frc.team4561.robot.automodes;
 
+import org.usfirst.frc.team4561.robot.Robot;
+
 import org.usfirst.frc.team4561.robot.commands.DriveArcadeTimed;
 import org.usfirst.frc.team4561.robot.commands.Fire;
 import org.usfirst.frc.team4561.robot.commands.PIDGoalAlign;
+import org.usfirst.frc.team4561.robot.commands.SetShooterSpeed;
 import org.usfirst.frc.team4561.robot.commands.ShooterAlign;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -13,15 +16,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoLowbarShootHigh extends CommandGroup {
     
     public  AutoLowbarShootHigh() {
+    	 
+    	// Drives forwards through low bar
+    	addSequential(new DriveArcadeTimed(-1, 0, 3));
     	// Align to goal prefer right
     	addSequential(new DriveArcadeTimed(0, 1, 1)); //TODO: must verify values
     	// Spin up shooter
-    	addSequential(new ShooterAlign());
+    	//TODO: let us test FIRST!!
     	// Fire
+    	addSequential(new SetShooterSpeed(1)); //TODO: verify
     	addSequential(new Fire());
-    	// Realign to drive backwards through low bar
-    	addSequential(new DriveArcadeTimed(0, -1, 1)); //TODO: Let us test FIRST!!
-    	// Drives backwards through low bar
-    	addSequential(new DriveArcadeTimed(-1, 0, 3));
     }
 }
